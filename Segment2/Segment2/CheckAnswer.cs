fab14c152c658d50b8643282e7e3bc7b8ecb5eda
Suwarnan <suwarnan@gmail.com>
2015-07-02 01:48:30 +0530

    Segment2 commit...

---
+++ Segment2/Segment2/CheckAnswer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Segment2
+{
+    class CheckAnswer
+    {
+        public string Check(Int32 answer)
+        {
+            string message = "Incorrect";
+
+            if (answer < 1 || answer > 100)
+            {
+                message = "Should be between 1 and 100";
+                return message;
+            }
+            
+            if (answer == 42)
+            {
+                message = "Correct";
+            }
+
+            return message;
+        }
+    }
+}
---
+++ Segment2/Segment2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Segment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer, message;
+
+            Console.WriteLine("Guess the answer please");
+
+            GetAnswer:
+            Console.Write("Answer :- ");
+
+            answer = Console.ReadLine();
+
+            int result;
+            if (!(int.TryParse(answer, out result)))
+            {
+                Console.WriteLine("The answer should be a number between 1 and 100");
+                Console.WriteLine("");
+                Console.WriteLine("Please try again");
+
+                goto GetAnswer;
+            }
+
+            CheckAnswer cheAnswer=new CheckAnswer();
+            message =cheAnswer.Check(Convert.ToInt32(answer));
+
+            Console.WriteLine(message);
+            Console.WriteLine("");
+
+            if (message != "Correct")
+            {
+                Console.WriteLine("Please try again");
+                goto GetAnswer;
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
